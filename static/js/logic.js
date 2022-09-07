0c6416757d2b819e4ed2389dd225b8978d57751d
Mark Jonse <markjonse90@Marks-MBP.lan>
2022-09-07 11:26:48 -0500

    13.4.3 Map Lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,8 +5,10 @@ console.log("working");
 let map = L.map("mapid",
                 {
                 // center: [37.5, -122.5],  // SFO
-                center: [34.0522, -118.2437],  // LA
-                 zoom:   11
+                //center: [34.0522, -118.2437],  // LA
+                //center: [36.1733, -120.1794], 
+                center: [37.6213, -122.3790],
+                 zoom:   5
                 });
 
 // // An array containing each city's location, state, and population.
@@ -42,22 +44,24 @@ let map = L.map("mapid",
 //   }
 //   ];
 
-  // Get data from cities.js
-let cityData = cities;
+//   // Get data from cities.js
+// let cityData = cities;
 
-   // Loop through the cities array and create one marker for each city.
-   cityData.forEach(function(city) {
-    console.log(city)
-    //L.marker(city.location)
-    L.circleMarker(city.location, {
-        radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+//    // Loop through the cities array and create one marker for each city.
+//    cityData.forEach(function(city) {
+//     console.log(city)
+//     //L.marker(city.location)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    
     //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -73,6 +77,21 @@ streets.addTo(map);
 //     fillColor: "#ffffa1"
 // }).addTo(map);
 
-L.circle([34.0522, -118.2437], {
-    radius: 100
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+
+ // 13.4.3 Map Lines
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
  }).addTo(map);
